f1d5855cd7789bde6d2aa6e4221841f3deda3106
robson.langkammer <robsonlangkammer@hotmail.com>
2025-02-04 20:25:50 -0300

    init

---
+++ backend/src/main/java/br/com/robson/showdeperguntas/ShowdeperguntasApplication.java
@@ -0,0 +1,13 @@
+package br.com.robson.showdeperguntas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowdeperguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowdeperguntasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/robson/showdeperguntas/ShowdeperguntasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robson.showdeperguntas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowdeperguntasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
